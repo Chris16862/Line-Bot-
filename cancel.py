8606dc841020b5870d37fcec0975decec18fc202
Apie0419 <xu3mp6xjp6@gmail.com>
2017-10-06 11:05:42 +0800

    test

--- cancel.py
+++ cancel.py
@@ -8,10 +8,9 @@ channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 line_bot_api = LineBotApi(channel_access_token)
 db = con.cursor()
 
-def get_reply(event,status,userid) :
+def get_reply(status,userid) :
     db.execute("DELETE FROM sell_list WHERE userid='{}' and status='{}'".format(userid, status[0][0]))
     con.commit()
     return TextSendMessage(
-    	event.reply_token,
     	text="取消成功"
     	)
