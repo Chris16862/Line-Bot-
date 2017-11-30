b5a589a7fa1ea89a9ccb698cf695480cab6f6452
Apie0419 <xu3mp6xjp6@gmail.com>
2017-12-01 01:17:06 +0800

    fix upload pic problems

--- func/actions/pic.py
+++ func/actions/pic.py
@@ -18,13 +18,15 @@ def save_pic(event,pic_id) :
             for chunk in message_content.iter_content():
                 fd.write(chunk)
         img = Image.open('pic.jpg')
-        img_size = img.size # (width, height)
-        if img_size[0] > img_size[1] :
-            p_size = (240, img_size[1]*(240/img_size[0]))
-            new_size = (1024, img_size[1]*(1024/img_size[0]))
+        width= img.width
+        height = img.height
+        if width > height :
+            p_size = [240, int(height*(240/width))]
+            new_size = [1024, int(height*(1024/width))]
         else : 
-            p_size = (img_size[0]*(240/img_size[1]), 240)
-            new_size = (img_size[0]*(1024/img_size[1]), 1024)
+            p_size = [int(width*(240/height)), 240]
+            new_size = [int(width*(1024/height)), 1024]
+        print (p_size,new_size)
         new_img= img.resize((p_size[0], p_size[1]),Image.ANTIALIAS)
         new_img.save('pic-p.jpg',quality=100)
         new_img = img.resize((1024, 650),Image.ANTIALIAS)
