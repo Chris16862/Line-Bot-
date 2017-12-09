0c42a4d239d33eba2086dbaace6b7350c3b3454e
chris <meter16862@gmail.com>
2017-12-09 19:48:04 +0800

    test

--- func/actions/buy.py
+++ func/actions/buy.py
@@ -33,17 +33,17 @@ def Buy(event, status, userid, con):
         return TextSendMessage(text="請輸入商品名稱:")
     elif status[0][0]=="enter_num":
         buy=event.message.text
-        db.execute("SELECT id,status,amount,price FROM sell_list WHERE name={}".format(buy))
+        db.execute("SELECT id,status,amount,price FROM sell_list WHERE id={}".format(buy))
         data=db.fetchall()
         if not data :
             db.close()
             return TextSendMessage(text="商品不存在，請重新輸入商品名稱\n若想取消本次交易，請按\"功能列表\"內的\"取消輸入\"")
-            elif data[0][1]=="check" :
-                db.close()
-                return TextSendMessage("本產品已經收單囉～\n請輸入其他商品編號")
-            elif data[0][2]<=0 :
-                db.close()
-                return TextSendMessage("本商品已售完~ \n請輸入其他商品編號")
+        elif data[0][1]=="check" :
+            db.close()
+            return TextSendMessage("本產品已經收單囉～\n請輸入其他商品編號")
+        elif data[0][2]<=0 :
+            db.close()
+            return TextSendMessage("本商品已售完~ \n請輸入其他商品編號")
         db.execute("DELETE FROM buy_list WHERE userid='{}' and status='enter_num'".format(userid))
         con.commit()
         db.close()
